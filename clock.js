d2572589a1c56af0acbe29df317faa20d7ad58cf
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-08 00:21:31 +0900

    index.html: form 추가

--- clock.js
+++ clock.js
@@ -1,5 +1,5 @@
 const clockContainer = document.querySelector(".js-clock");
-let clockTitle = clockContainer.querySelector("h1");
+const clockTitle = clockContainer.querySelector("h1");
 
 function getTime() {
   const date = new Date();
@@ -7,8 +7,7 @@ function getTime() {
   const minute = date.getMinutes();
   const second = date.getSeconds();
   const toTwoDigit = (number) => {
-    return (number <= 9 ? '0' + number: number);
-    
+    return (number <= 9 ? '0' + number: number); 
   }
   clockTitle.innerText = `${toTwoDigit(hour)}:${toTwoDigit(minute)}:${toTwoDigit(second)}`;
 }
