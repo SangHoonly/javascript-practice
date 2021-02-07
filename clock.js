1b48ea3db243c3efdd0baa843971ce0855c17cbd
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-07 23:21:20 +0900

    clock.js: 추가 및 시계 기능 작동

---
+++ clock.js
@@ -0,0 +1,17 @@
+const clockContainer = document.querySelector(".js-clock");
+let clockTitle = clockContainer.querySelector("h1");
+
+function getTime() {
+  const date = new Date();
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const second = date.getSeconds();
+  const toTwoDigit = (number) => {
+    return (number <= 9 ? '0' + number: number);
+    
+  }
+  clockTitle.innerText = `${toTwoDigit(hour)}:${toTwoDigit(minute)}:${toTwoDigit(second)}`;
+}
+
+
+setInterval(getTime, 1000);
