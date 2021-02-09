f6e643134a62d79a51ea66fc0c2c21affa7b8390
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-09 22:06:41 +0900

    greeting.js 구현

--- greeting.js
+++ greeting.js
@@ -5,16 +5,41 @@ const greeting = document.querySelector(".js-greetings");
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
 
-function paintGreeting(text) {
+function saveNameToStorage(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  showGreeting(currentValue);
+  saveNameToStorage(currentValue);
+  input.value = '';
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function showGreeting(text) {
   form.classList.remove(SHOWING_CN);
   greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`
+  greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
   const currentUser = localStorage.getItem(USER_LS);
-  if (currentUser === null) {
-  } else {
-    paintGreeting(currentUser);
+  if (currentUser === "") {
+    askForName();
+    return;
   }
+  showGreeting(currentUser);
 }
+
+function init() {
+  localStorage.setItem(USER_LS, "");
+  loadName();
+}
+
+init();
--- practice.js
+++ practice.js
@@ -1,5 +1,30 @@
-const prices = ['1.0', 'negotiable', '2.15'];
+const dogs = [
+  {
+    이름: '맥스',
+    크기: '소형견',
+    견종: '보스턴테리어',
+    색상: '검정색',
+  },
+  {
+    이름: '도니',
+    크기: '대형견',
+    견종: '래브라도레트리버',
+    색상: '검정색',
+  },
+  {
+  이름: '섀도',
+  크기: '중형견',
+  견종: '래브라도레트리버',
+  색상: '갈색',
+  },
+];
+// 갈색 색상만 빼고 나머지 색깔 출력
+const colors = dogs.reduce((colors, dog) => {
+  // console.log(dog.색상);
+  // if (dog.색상 === '갈색') {
+  //   return 
+  // }
+  return (dog.색상 !== '갈색' ? [...colors, dog['색상']] : colors);
+}, []);
 
-const formattedPrices = 
-  prices.filter(price => parseFloat(price)).map(price => parseFloat(price));
-console.log(formattedPrices);
+console.log(colors);
