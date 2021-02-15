3e7af43888a8fda67a3ae2ce277fcf6a7be885bc
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-15 22:06:15 +0900

    nomad JS: #3.5

--- todo.js
+++ todo.js
@@ -4,15 +4,29 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
 function addToDo(text) {
   const li = document.createElement("li");
   const deleteButton = document.createElement("button");
   const span = document.createElement("span");
+  const newId = toDos.length + 1;
   deleteButton.innerText = "완료";
   span.innerText = text;
   li.appendChild(span);
   li.appendChild(deleteButton);
+  li.id = newId;
   toDoList.appendChild(li);
+  const toDoObj = {
+    text: text,
+    id: newId
+  };
+  toDos.push(toDoObj);
+  saveToDos();
 }
 
 function handleSubmit(event) {
@@ -23,7 +37,13 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-  const toDos = localStorage.getItem(TODOS_LS);
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+    const parsedToDos = JSON.parse(loadedToDos);
+    parsedToDos.forEach(toDo => {
+      addToDo(toDo.text);
+    });
+  }
 }
 
 function init() {
