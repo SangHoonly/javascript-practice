e2434bc9d0a3848a1d4c992b6be2cc163fbdda75
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-16 22:14:38 +0900

    todo.js: changed

--- todo.js
+++ todo.js
@@ -50,6 +50,7 @@ function handleSubmit(event) {
 
 function loadToDos() {
   const loadedToDos = localStorage.getItem(TODOS_LS);
+  console.log(loadToDos);
   if (loadedToDos !== null) {
     const parsedToDos = JSON.parse(loadedToDos);
     parsedToDos.forEach(toDo => {
