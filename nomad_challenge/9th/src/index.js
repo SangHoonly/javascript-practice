f442ec038ca1fa0c37573a1ef348b516668d9c5b
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-18 23:56:47 +0900

    8th challenge: implemented numberbutton & reflect number change

--- nomad_challenge/9th/src/index.js
+++ nomad_challenge/9th/src/index.js
@@ -1,9 +1,22 @@
 // <⚠️ DONT DELETE THIS ⚠️>
-import "./styles.css";
+// import "./styles.css";
 // <⚠️ /DONT DELETE THIS ⚠️>
 
-let resultNumber = document.getElementById("resultNumber");
-
 function handleNumberClick(event) {
+  // const number = parseInt(event.target.value);
   document.getElementById("resultNumber").innerText = event.target.value;
 }
+
+let resultNumber = document.getElementById("resultNumber").innerText;
+
+const numberButtonContainer = document.getElementById("numberButtonContainer");
+const numberButtons = [1,2,3,4,5,6,7,8,9,0].map((number) => {
+  const numberButton = document.createElement("button");
+  numberButton.value = number;
+  numberButton.innerText = number;
+  numberButton.addEventListener('click', handleNumberClick);
+  numberButtonContainer.appendChild(numberButton);
+  return numberButton;
+});
+
+
