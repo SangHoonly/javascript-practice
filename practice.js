24400ef8a505630006f656c156e986815e5953bc
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-23 22:57:40 +0900

    fetch: 연습중..

--- practice.js
+++ practice.js
@@ -1,30 +1,16 @@
-const dogs = [
-  {
-    이름: '맥스',
-    크기: '소형견',
-    견종: '보스턴테리어',
-    색상: '검정색',
-  },
-  {
-    이름: '도니',
-    크기: '대형견',
-    견종: '래브라도레트리버',
-    색상: '검정색',
-  },
-  {
-  이름: '섀도',
-  크기: '중형견',
-  견종: '래브라도레트리버',
-  색상: '갈색',
-  },
-];
-// 갈색 색상만 빼고 나머지 색깔 출력
-const colors = dogs.reduce((colors, dog) => {
-  // console.log(dog.색상);
-  // if (dog.색상 === '갈색') {
-  //   return 
-  // }
-  return (dog.색상 !== '갈색' ? [...colors, dog['색상']] : colors);
-}, []);
+const fetch = require("node-fetch");
+async function loadData() {
+  const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+  const response = await fetch(url);
+  const data = await response.json();
+}
 
-console.log(colors);
+
+  // const url = 'https://eatgo-customer-api.ahastudio.com/regions';
+
+// fetch("https://jsonplaceholder.typicode.com/posts/1").then((response) =>
+//   console.log(response)
+// )
+
+const data = loadData();
+console.log(data);
