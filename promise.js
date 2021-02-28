fb01319800e89628abaf3a747ddaca7e426d397d
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-02-28 21:12:28 +0900

    promise practice

--- promise.js
+++ promise.js
@@ -1,20 +1,10 @@
 var _promise = function (param) {
 	return new Promise(function (resolve, reject) {
-		// 비동기를 표현하기 위해 setTimeout 함수를 사용 
 		window.setTimeout(function () {
-
-			// 파라메터가 참이면, 
-			if (param) {
-
-				// 해결됨 
-				resolve("해결 완료");
-			}
-
-			// 파라메터가 거짓이면, 
-			else {
-
-				// 실패 
-				reject(Error("실패!!"));
+			if (param) { 
+        resolve("해결 완료");
+			}	else {
+        reject(Error("실패!!"));
 			}
 		}, 3000);
 	});
