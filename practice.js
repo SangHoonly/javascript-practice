a4bac225fffd9812b0fb2cfdf0f284487b59b5b2
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-02 23:51:01 +0900

    부분 적용 함수

--- practice.js
+++ practice.js
@@ -6,6 +6,40 @@ const sayAge = lsh(28);
 console.log(sayAge);
 sayAge();
 
+const building = {
+  hours: '8am ~ 8pm',
+  address: '대구시 수성구',
+};
+
+const manager = {
+  name: '이상훈',
+  phone: '01031325664',
+}
+
+const program = {
+  name: '프로그램',
+  room: '321',
+  hours: '22 ~ 22',
+}
+
+function mergeProgramInformation(building, manager) {
+  const { hours, address } = building;
+  const { name, phone } = manager;
+  const defaults = {
+    hours,
+    address,
+    contact: name,
+    phone,
+  };
+
+  return program => {
+    return { ...defaults, ...program };
+  };
+}
+
+const programInfo = mergeProgramInformation(building, manager)(program);
+console.log(programInfo);
+
 // const tenPercentOff = discounter(0.1);
 // console.log(tenPercentOff(100));
 
